791340844885cbcedff8e2761b6b62e4c28ec197
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-11-13 14:18:55 +0100

    added postgres connexion instead of Sqlite

--- api/database/db.ts
+++ api/database/db.ts
@@ -9,22 +9,22 @@ import { Lang } from "../src/languages/language.entity";
 dotenv.config();
 
 
-export const dataSource = new DataSource({
-    type: "sqlite",
-    database: "./database/repomanager.db",
-    entities: [Repo, Status, Lang],
-    synchronize: true,
-    logging:false, // permet de voir les opérations SQL qui sont faites
-});
-
 // export const dataSource = new DataSource({
-//     type: "postgres",
-//     host: process.env.DB_HOST, // Nom de l'image associé à Postgres --name dans la commande
-//     port: 5432,
-//     username: "postgres",
-//     password: process.env.POSTGRES_PASSWORD, // -e POSTGRES_PASSWORD=
-//     database: "postgres",
+//     type: "sqlite",
+//     database: "./database/repomanager.db",
 //     entities: [Repo, Status, Lang],
 //     synchronize: true,
-//   });
+//     logging:false, // permet de voir les opérations SQL qui sont faites
+// });
+
+export const dataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST, // Nom de l'image associé à Postgres --name dans la commande
+    port: 5432,
+    username: "postgres",
+    password: process.env.POSTGRES_PASSWORD, // -e POSTGRES_PASSWORD=
+    database: "postgres",
+    entities: [Repo, Status, Lang],
+    synchronize: true,
+  });
    
